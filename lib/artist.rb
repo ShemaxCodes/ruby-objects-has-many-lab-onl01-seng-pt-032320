44dfac676e0ea84eaa863e4d0004da25af246d39
Jashema Panapa <jcpanapa@gmail.com>
2020-04-17 15:23:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,5 @@
 class Artist
-
-  attr_accessor :name, :songs
+  attr_accessor :name, :songs 
 
   @@song_count = 0
 
@@ -12,18 +11,20 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@song_count += 1
+  end
+
+  def songs
+    @songs 
   end
 
   def self.song_count
     @@song_count
   end
-
 end
